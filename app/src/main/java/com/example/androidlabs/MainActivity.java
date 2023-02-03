d65f03f2f5768ac699389f40ad67d59aa217f8a7
myha <myhadev@gmail.gmailcom>
2023-02-02 21:50:46 -0400

    Lab 4

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,57 +1,111 @@
 package com.example.androidlabs;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
-            if (resultCode == 1) {
-                finish();
-            }
+    static class object {
+        String text;
+        Boolean urgent;
+        public object(String text, Boolean urgent) {
+            this.text = text;
+            this.urgent = urgent;
         }
     }
+    ArrayList<object> elements = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
 
-// The value will be default as empty string because for
-// the very first time when the app is opened, there is nothing to show
-        String welcome = sh.getString("welcome", "");
+        class MyListAdapter extends BaseAdapter {
 
-// We can then use the data
-        EditText editText = findViewById(R.id.editText);
-        editText.setText(welcome);
-        Button button = findViewById(R.id.button);
+            public int getCount() {
+                return elements.size();}
 
-        button.setOnClickListener( click -> {
-            Intent nextPage = new Intent(this,NameActivity.class);
-            String content = editText.getText().toString();
-            nextPage.putExtra("welcome",content);
-            startActivityForResult(nextPage,0);
+            public Object getItem(int position) {
+                return position;}
+
+            public long getItemId(int position) {
+                return (long) position;}
+
+            public View getView(int position, View old, ViewGroup parent) {
+                View newView = old;
+                LayoutInflater inflater = getLayoutInflater();
+                if (newView == null) {
+                    newView = inflater.inflate(R.layout.row_layout, parent, false);
+                }
+                TextView tView = newView.findViewById(R.id.textGoesHere);
+                tView.setText(elements.get(position).text);
+                if (elements.get(position).urgent) {
+                    tView.setBackgroundColor(Color.RED);
+                    tView.setTextColor(Color.WHITE);
+                } else {
+                    tView.setBackgroundColor(Color.WHITE);
+                    tView.setTextColor(Color.BLACK);
+                }
+                return newView;
+            }
+        }
+
+        ListView todolist = findViewById(R.id.toDoList);
+        MyListAdapter myAdapter = new MyListAdapter();
+        todolist.setAdapter(myAdapter);
+
+
+        todolist.setOnItemLongClickListener( (parent, view, position, id) -> {
+            View extraViewStuff = getLayoutInflater().inflate(R.layout.row_layout, null) ;
+            String extraText = R.string.rowline + String.valueOf(position);
+            ((TextView)extraViewStuff.findViewById(R.id.textGoesHere)).setText(extraText);
+
+                    AlertDialog.Builder aDialog = new AlertDialog.Builder(this);
+                    aDialog.setMessage(R.string.delete)
+                            .setPositiveButton(R.string.yes, (click, arg) -> {
+                                elements.remove(position);
+                                myAdapter.notifyDataSetChanged();
+                            })
+                            .setNegativeButton(R.string.no, (click, arg) -> {
+                            })
+                            .setView(extraViewStuff)
+                            .create()
+                            .show();
+            return true;
         });
 
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
-        SharedPreferences.Editor myEdit = sharedPreferences.edit();
-        EditText editText = findViewById(R.id.editText);
-        myEdit.putString("welcome", editText.getText().toString());
-        myEdit.commit();
+        Button addButton = findViewById(R.id.button);
+        Switch sw = findViewById(R.id.urgent);
+        sw.setChecked(false);
+        addButton.setOnClickListener(click ->
+        {
+            EditText addElement = findViewById(R.id.typeHere);
+            object obj = new object(addElement.getText().toString(),sw.isChecked());
+            if (sw.isChecked()) {
+                sw.setChecked(false);
+            }
+            elements.add(obj);
+            addElement.setText(null);
+            myAdapter.notifyDataSetChanged();
+        });
+
+        SwipeRefreshLayout swiperefresh = findViewById(R.id.swiperefresh);
+        swiperefresh.setOnRefreshListener(()-> swiperefresh.setRefreshing(false));
+
     }
     }
